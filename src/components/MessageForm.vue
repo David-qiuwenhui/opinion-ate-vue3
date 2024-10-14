e9d8fc904469c3b4fd22c29e0a59a59e05fcd5c9
qiuwenhui <1059713136@qq.com>
2024-10-14 23:43:08 +0800

    ✨ feat: 完成 MessageForm 数据输入部分

---
+++ src/components/MessageForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <input type="text" data-test="messageText" v-model="message" />
+    <button @click="send">send</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const message = ref("");
+function send() {
+  message.value = "";
+}
+</script>
+
+<style lang="scss" scoped></style>
