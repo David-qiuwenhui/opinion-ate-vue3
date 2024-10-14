86f0ef8d44ee54d299de2ebf8e7452c24e8e4b42
qiuwenhui <1059713136@qq.com>
2024-10-14 23:04:28 +0800

    ✨ feat: init cypress

---
+++ src/components/Button.cy.js
@@ -0,0 +1,9 @@
+import Button from "./Button.vue";
+
+describe("<Button />", () => {
+  it("renders", () => {
+    // see: https://on.cypress.io/mounting-vue
+    cy.mount(Button);
+    cy.contains("btn").click();
+  });
+});
---
+++ src/components/Button.vue
@@ -0,0 +1,7 @@
+<template>
+  <button>btn</button>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
